e5d9cd78a62cca61689a8459fdb6696c949b0418
Andres Born <andres.born@gmail.com>
2026-09-10 14:02:12 -0600

    Standardize returns for options service

--- service/options.ts
+++ service/options.ts
@@ -1,68 +1,66 @@
 import { db } from "@/db/drizzle";
 import { option } from "@/db/schema";
 import { optionInsertSchema } from "@/db/schema/option";
+import { AppError, err, ok, Result } from "@/lib/result";
 import { eq } from "drizzle-orm";
 import z from "zod";
+import { Option } from "@/db/types";
 
 export const createOption = async (
   insertData: { pollId: string; label: string; userId: string },
-) => {
+): Promise<Result<Option[]>> => {
   const { userId, ...rest } = insertData;
   const parsed = optionInsertSchema.safeParse(rest);
 
   if (!parsed.success) {
-    return {
-      success: false,
-      result: null,
-      error: z.treeifyError(parsed.error),
-    };
+    return err(
+      AppError.badRequest(
+        "Invalid option insert payload.",
+        z.treeifyError(parsed.error),
+      ),
+    );
   }
 
   const { pollId, label } = parsed.data;
-
-  const userPoll = await db.query.poll.findFirst({
-    where: { id: pollId, userId },
-  });
-  if (!userPoll) {
-    return {
-      success: false,
-      result: null,
-      error: new Error("Create Option: User does not own poll."),
-    };
-  }
-
   try {
+    const userPoll = await db.query.poll.findFirst({
+      where: { id: pollId, userId },
+    });
+
+    if (!userPoll) {
+      return err(AppError.forbidden("User does not own this poll."));
+    }
+
     const result = await db.insert(option).values({ pollId, label })
       .returning();
-    return { success: true, result, error: null };
+    return ok(result);
   } catch (e) {
     console.error(e);
-    return { success: false, result: null, error: e };
+    return err(AppError.internalServerError("Failed to insert option."));
   }
 };
 
-export const deleteUserOption = async (optionId: string, userId: string) => {
-  const result = await db.query.option.findFirst({
-    where: { id: optionId },
-    with: { poll: true },
-  });
-  if (result && result.poll?.userId === userId) {
-    return await deleteOption(optionId);
-  }
-  return {
-    success: false,
-    result: null,
-    error: new Error("Delete Option: User does no own poll."),
-  };
-};
-
-export const deleteOption = async (id: string) => {
+export const deleteUserOption = async (
+  optionId: string,
+  userId: string,
+): Promise<Result<Option[]>> => {
   try {
-    const result = await db.delete(option).where(eq(option.id, id))
-      .returning();
-    return { success: true, result, error: null };
+    const result = await db.query.option.findFirst({
+      where: { id: optionId },
+      with: { poll: true },
+    });
+    if (result && result.poll?.userId === userId) {
+      const toDelete = await db.delete(option).where(eq(option.id, optionId))
+        .returning();
+      return ok(toDelete);
+    }
+    return err(
+      AppError.forbidden(
+        "Failed to delete option, user doesn't own this poll.",
+      ),
+    );
   } catch (e) {
     console.error(e);
-    return { success: false, result: null, error: e };
+    return err(AppError.internalServerError("Failed to delete option."));
   }
 };
